b5798b7dc58c0ae83854664383dcba67b4f4be22
a <stepanovsm8@gmail.com>
2024-08-09 11:59:38 +0300

    cell

--- Cell.cpp
+++ Cell.cpp
@@ -17,3 +17,21 @@ void Cell::
   if (isAlive && (n > 3 || n < 2))
     isAlive = false;
 }
+
+bool Cell::operator==(const Cell &c) const noexcept {
+  return x == c.x && y == c.y && isAlive == c.isAlive;
+}
+
+// Implementing the '<' operator
+bool Cell::operator<(const Cell &c) const noexcept {
+  if (x < c.x)
+    return true;
+  if (x == c.x && y < c.y)
+    return true;
+  return false;
+}
+
+// Implementing the '>' operator
+bool Cell::operator>(const Cell &c) const noexcept {
+  return !(*this < c) && !(*this == c);
+}
